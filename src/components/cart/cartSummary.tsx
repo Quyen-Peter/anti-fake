9cc697432c098b4e4970540e38497f4f43249343
Quyen-Peter <quyentrinh040@gmail.com>
2026-06-13 16:30:39 +0700

    fix sumary cart mobile

--- src/components/cart/cartSummary.tsx
+++ src/components/cart/cartSummary.tsx
@@ -1,3 +1,6 @@
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { useState } from "react";
+
 type Props = {
   subtotal: number;
   discount: number;
@@ -5,45 +8,50 @@ type Props = {
 };
 
 export default function CartSummary({ subtotal, discount, total }: Props) {
+  const [showDetail, setShowDetail] = useState(false);
   const formatPrice = (price: number) =>
     new Intl.NumberFormat("vi-VN").format(price);
   return (
     <div className="cart-summary">
+      <button className="detail-btn" onClick={() => setShowDetail(!showDetail)}>
+        {showDetail ? <ChevronUp size={22} /> : <ChevronDown size={22} />}
+      </button>     
       <h2>Tóm tắt đơn hàng</h2>
+      <div className={`summary-detail ${showDetail ? "show" : ""}`}>
+        <div className="summary-row">
+          <span>Tạm tính</span>
+          <b>3{formatPrice(subtotal)}đ</b>
+        </div>
 
-      <div className="summary-row">
-        <span>Tạm tính</span>
-        <b>3{formatPrice(subtotal)}đ</b>
+        <div className="summary-row">
+          <span>Giảm giá sản phẩm</span>
+          <span className="discount">-{formatPrice(discount)}đ</span>
+        </div>
+
+        <div className="summary-row">
+          <span>Phí vận chuyển</span>
+          <span className="free">Miễn phí</span>
+        </div>
+
+        <hr />
       </div>
-
-      <div className="summary-row">
-        <span>Giảm giá sản phẩm</span>
-        <span className="discount">-{formatPrice(discount)}đ</span>
-      </div>
-
-      <div className="summary-row">
-        <span>Phí vận chuyển</span>
-        <span className="free">Miễn phí</span>
-      </div>
-
-      <hr />
-
       <div className="summary-total">
         <span>Tổng cộng</span>
         <b>{formatPrice(total)}đ</b>
       </div>
-      <div className="cart-bnt-payment">
-        <div className="coupon-box">
-          <label>MÃ GIẢM GIÁ</label>
+      <div className={`summary-detail ${showDetail ? "show" : ""}`}>
+        <div className="cart-bnt-payment">
+          <div className="coupon-box">
+            <label>MÃ GIẢM GIÁ</label>
 
-          <div className="coupon-input">
-            <input placeholder="Nhập mã..." />
-            <button>Áp dụng</button>
+            <div className="coupon-input">
+              <input placeholder="Nhập mã..." />
+              <button>Áp dụng</button>
+            </div>
           </div>
         </div>
-
-        <button className="checkout-btn">Tiến hành thanh toán</button>
       </div>
+      <button className="checkout-btn">Tiến hành thanh toán</button>
     </div>
   );
 }

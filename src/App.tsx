75f076b367f18a01a8b70c3ba62938fe6206e176
Quyen-Peter <quyentrinh040@gmail.com>
2026-06-17 15:40:56 +0700

    fix app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import ShopPage from "./pages/shop";
 import ShopProducts from "./components/shop/shopProducts";
 import ShopCategories from "./components/shop/shopCategories";
 import ShopReviews from "./components/shop/shopReviews";
-import MessageRoomPage from "./pages/message/room";
 
 function App() {
   return (
